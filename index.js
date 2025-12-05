8a6e6b747821532adfeff7e08286e172867b95d4
Anatoliy <github.com/tolikhere>
2025-12-05 20:27:37 +0300

    Update routes now they handles editting and saving new posts, also added app.param for the id variable

--- index.js
+++ index.js
@@ -7,26 +7,66 @@ const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 
+const posts = [];
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-	res.render('index', { message: 'Home Page' });
+	res.render('index', { posts: posts });
 });
 
-app.get('/articles', (req, res) => {
-	res.render('index', { message: 'Articles' });
+app.post('/new-post', (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+	posts.push({
+		title: req.body.title,
+		message: req.body.message,
+		id: Date.now().toString(),
+	});
+	res.redirect('/');
 });
 
-app.get('/edit', (req, res) => {
-	res.render('index', { message: 'Edit' });
+app.param('id', (req, res, next, id) => {
+	if (/^\d+$/.test(id)) {
+		next();
+	} else {
+		res.status(404).send('Invalid Post ID format');
+	}
+});
+
+app.get('/edit/:id', (req, res) => {
+	const postId = req.params.id;
+	const postToEdit = posts.find(p => p.id === postId);
+	if (postToEdit) {
+		res.render('edit-form', { post: postToEdit });
+	} else {
+		res.status(404).send('Post not found');
+	}
+});
+
+app.post('/edit/:id', (req, res) => {
+	const postId = req.params.id;
+	const { title, message } = req.body;
+
+	const postIndex = posts.findIndex(p => p.id === postId);
+
+	if (postIndex !== -1) {
+		posts[postIndex].title = title;
+		posts[postIndex].message = message;
+		console.log(`Post ${postId} updated successfully.`);
+
+		res.redirect('/');
+	} else {
+		res.status(404).send('Post not found');
+	}
 });
 
 app.use((req, res) => {
-	res.render('index', { message: 'Sorry, we couldn\'t find that page!' });
+	res.status(404).send('Not found');
 });
 
 app.listen(port, () => {
